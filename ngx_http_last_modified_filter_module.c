5120eb9ca67794cc2da89c537628ab8fd497bddb
Mikalai Ananenka <git@3dyd.com>
2019-10-21 08:38:36 -0400

    refactor following nginx practices

--- ngx_http_last_modified_filter_module.c
+++ ngx_http_last_modified_filter_module.c
@@ -68,29 +68,44 @@ ngx_module_t ngx_http_last_modified_filter_module = {
 
 static ngx_http_output_header_filter_pt  ngx_http_next_header_filter;
 
-static ngx_int_t update_header(ngx_http_request_t *r, ngx_http_complex_value_t *source, ngx_flag_t update_etag)
+static ngx_int_t
+ngx_http_last_modified_header_filter(ngx_http_request_t *r)
 {
-    ngx_str_t uri, path;
-    ngx_http_core_loc_conf_t *clcf;
-    u_char* last;
-    ngx_open_file_info_t of;
+    u_char                             *last;
+    ngx_str_t                           uri, path;
+    ngx_open_file_info_t                of;
+    ngx_http_core_loc_conf_t           *clcf;
+    ngx_http_last_modified_loc_conf_t  *sllc;
 
-    if (ngx_http_complex_value(r, source, &uri) != NGX_OK) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: could not generate last_modified_source value");
-        return -1;
+    if (r != r->main || !(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
+        return ngx_http_next_header_filter(r);
+    }
+
+    sllc = ngx_http_get_module_loc_conf(r, ngx_http_last_modified_filter_module);
+
+    if (!sllc->enable || !sllc->source) {
+        return ngx_http_next_header_filter(r);
+    }
+
+    if (sllc->clear_etag) {
+        ngx_http_clear_etag(r);
+    }
+
+    if (ngx_http_complex_value(r, sllc->source, &uri) != NGX_OK) {
+        return NGX_ERROR;
     }
 
     clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
 
     path.len = clcf->root.len + uri.len + 1;
-    path.data = ngx_pcalloc(r->pool, path.len);
+    path.data = ngx_pnalloc(r->pool, path.len);
     if (path.data == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: could not allocate memory for full path");
-        return -2;
+        return NGX_ERROR;
     }
 
     last = ngx_copy(path.data, clcf->root.data, clcf->root.len);
     last = ngx_copy(last, uri.data, uri.len);
+    *last = 0;
 
     ngx_memzero(&of, sizeof(ngx_open_file_info_t));
     of.read_ahead = clcf->read_ahead;
@@ -100,53 +115,31 @@ static ngx_int_t update_header(ngx_http_request_t *r, ngx_http_complex_value_t *
     of.errors = clcf->open_file_cache_errors;
     of.events = clcf->open_file_cache_events;
 
-    if (ngx_http_set_disable_symlinks(r, clcf, &path, &of) == NGX_OK) {
-        if (ngx_open_cached_file(clcf->open_file_cache, &path, &of, r->pool) == NGX_OK) {
-            if (of.is_file) {
-                if (r->headers_out.last_modified_time < of.mtime) {
-                    r->headers_out.last_modified_time = of.mtime;
+    if (ngx_http_set_disable_symlinks(r, clcf, &path, &of) != NGX_OK) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
 
-                    if (update_etag && -1 != of.size) {
-                        off_t prev_size = r->headers_out.content_length_n;
-                        r->headers_out.content_length_n = of.size;
-                        r->headers_out.last_modified_time = of.mtime;
-                        ngx_http_set_etag(r);
-                        r->headers_out.content_length_n = prev_size;
-                    }
+    if (ngx_open_cached_file(clcf->open_file_cache, &path, &of, r->pool) == NGX_OK) {
+        if (of.is_file) {
+            if (r->headers_out.last_modified_time < of.mtime) {
+                r->headers_out.last_modified_time = of.mtime;
+
+                if (!sllc->clear_etag && -1 != of.size) {
+                    off_t prev_size = r->headers_out.content_length_n;
+                    r->headers_out.content_length_n = of.size;
+                    r->headers_out.last_modified_time = of.mtime;
+                    ngx_http_set_etag(r);
+                    r->headers_out.content_length_n = prev_size;
                 }
-            }
-            else {
-                ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: %V is not a file", &path);
             }
         }
         else {
-            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: could not open %V", &path);
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: %V is not a file", &path);
         }
     }
     else {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: count not disable symlinks on %V", &path);
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "last_modified: could not open %V", &path);
     }
-
-    ngx_pfree(r->pool, path.data);
-
-    return 0;
-}
-
-static ngx_int_t
-ngx_http_last_modified_header_filter(ngx_http_request_t *r)
-{
-    ngx_http_last_modified_loc_conf_t *sllc;
-    sllc = ngx_http_get_module_loc_conf(r, ngx_http_last_modified_filter_module);
-
-    if (!sllc->enable || !sllc->source || r != r->main || !(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
-        return ngx_http_next_header_filter(r);
-    }
-
-    if (sllc->clear_etag) {
-        ngx_http_clear_etag(r);
-    }
-
-    update_header(r, sllc->source, !sllc->clear_etag);
 
     return ngx_http_next_header_filter(r);
 }
